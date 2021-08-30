4650db9880717caf2e711a037b31fdf0885fb401
ippee <ippee.music@gmail.com>
2021-08-30 17:27:28 +0900

    下限と上限の値チェックの追加

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,19 @@
 const cutup = () => {
-  console.log("ok");
+  let srcText = document.getElementById("src").value;
+  let start = document.getElementById("start").value.split(",");
+  let end = document.getElementById("end").value.split(",");
+  let middle = document.getElementById("middle").value.split(",");
+  let lower = parseInt(document.getElementById("lower").value);
+  let upper = parseInt(document.getElementById("upper").value);
+
+  if (lower > upper) {
+    alert(
+      "下限よりも上限の値の方が小さくなっています。\n上限の値の方が大きくなるよう設定してください。"
+    );
+    return;
+  }
+
+  console.log(srcText, start, end, middle, lower, upper);
 };
 
 window.cutup = cutup;
