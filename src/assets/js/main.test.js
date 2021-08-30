f30caa223edc730b60038253ae94df3270ffa3df
ippee <ippee.music@gmail.com>
2021-08-30 22:02:28 +0900

    テストの修正
    1. srcTextの修正。ひらがなが連続するような文章はtiny-segmenterに不向き
    2. splitPointの形式を、セグメントを入れたときにその番号を返すように修正

--- src/assets/js/main.test.js
+++ src/assets/js/main.test.js
@@ -1,49 +1,46 @@
 import { cutup } from "./main";
 
 test("文章の分割", () => {
-  let srcText = "すもももももももものうち";
+  let srcText = "隣の客はよく柿食う客だ";
   let splitPoint = {
-    start: ["「", "（"],
-    end: ["。", "」", "）", "？"],
-    middle: [
-      "は",
-      "へ",
-      "を",
-      "の",
-      "で",
-      "から",
-      "に",
-      "て",
-      "が",
-      "も",
-      "、",
-    ],
+    "「": 0,
+    "（": 0,
+    "。": 1,
+    "」": 1,
+    "）": 1,
+    "？": 1,
+    は: 2,
+    へ: 3,
+    を: 4,
+    の: 5,
+    で: 6,
+    から: 7,
+    に: 8,
+    て: 9,
+    が: 10,
+    も: 11,
+    "、": 12,
   };
+
+  cutup.manualInit(srcText, splitPoint, 20, 30);
 
   let expected = [
     {
-      value: "すももも",
+      value: "隣",
       head: 0,
-      tail: 11,
-    },
-    {
-      value: "ももも",
-      head: 11,
-      tail: 11,
-    },
-    {
-      value: "ももの",
-      head: 11,
       tail: 5,
     },
     {
-      value: "うち",
+      value: "の客",
       head: 5,
+      tail: 2,
+    },
+    {
+      value: "はよく柿食う客だ",
+      head: 2,
       tail: 1,
     },
   ];
-
-  cutup.manualInit(srcText, splitPoint, 20, 30);
 
   expect(cutup.splitText()).toStrictEqual(expected);
 });
