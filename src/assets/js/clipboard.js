5a2c0dba12c45c21195fd25c4af89f08025024f8
ippee <ippee.music@gmail.com>
2021-09-03 10:24:22 +0900

    execCommandを避けた書き方に変更

--- src/assets/js/clipboard.js
+++ src/assets/js/clipboard.js
@@ -3,9 +3,8 @@ import { popover } from "./popover";
 document.getElementById("copy").addEventListener(
   "click",
   () => {
-    let textarea = document.getElementById("result");
-    textarea.select();
-    document.execCommand("copy");
+    let text = document.getElementById("result").value;
+    navigator.clipboard.writeText(text);
     popover("popover-copy");
   },
   false
