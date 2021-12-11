0b21956c900415e504c3b8093525ef219b3d85f0
ichi-h <85932406+ichi-h@users.noreply.github.com>
2021-12-11 14:33:25 +0900

    updateのテストを追加

---
+++ src/assets/js/mvu/mvu.test.js
@@ -0,0 +1,95 @@
+import { update } from "./update";
+import { model } from "./model";
+
+jest.mock("./view", () => ({
+  view: jest.fn((model, targets) => [model, targets]),
+}));
+
+describe("update", () => {
+  describe("Change", () => {
+    const dispatch = update(model);
+
+    test("srcの更新", () => {
+      const [newModel, targets] = dispatch({
+        type: "Change",
+        target: "src",
+        newValue: "すもももももももものうち",
+      });
+      expect(newModel.src).toBe("すもももももももものうち");
+      expect(targets.length).toBe(0);
+    });
+
+    test("startの更新", () => {
+      const [newModel, targets] = dispatch({
+        type: "Change",
+        target: "start",
+        newValue: "#",
+      });
+      expect(newModel.start).toBe("#");
+      expect(targets.length).toBe(0);
+    });
+
+    test("endの更新", () => {
+      const [newModel, targets] = dispatch({
+        type: "Change",
+        target: "end",
+        newValue: "#",
+      });
+      expect(newModel.end).toBe("#");
+      expect(targets.length).toBe(0);
+    });
+
+    test("middleの更新", () => {
+      const [newModel, targets] = dispatch({
+        type: "Change",
+        target: "middle",
+        newValue: "#",
+      });
+      expect(newModel.middle).toBe("#");
+      expect(targets.length).toBe(0);
+    });
+
+    test("lowerの更新", () => {
+      const [newModel, targets] = dispatch({
+        type: "Change",
+        target: "lower",
+        newValue: 0,
+      });
+      expect(newModel.lower).toBe(0);
+      expect(targets.length).toBe(0);
+    });
+
+    test("upperの更新", () => {
+      const [newModel, targets] = dispatch({
+        type: "Change",
+        target: "upper",
+        newValue: 0,
+      });
+      expect(newModel.upper).toBe(0);
+      expect(targets.length).toBe(0);
+    });
+  });
+
+  test("Cutup (文章生成)", () => {
+    const [newModel, targets] = update(model)({
+      type: "Cutup",
+    });
+
+    expect(
+      newModel.lower <= newModel.result.length &&
+        newModel.result.length <= newModel.upper
+    ).toBe(true);
+    expect(targets.includes("result")).toBe(true);
+  });
+
+  describe("異常系", () => {
+    test("不明なメッセージを受け取った時", () => {
+      try {
+        update(model)({ type: "Error" });
+      } catch (_) {
+        return;
+      }
+      throw Error("不明なメッセージが通過しています。");
+    });
+  });
+});
